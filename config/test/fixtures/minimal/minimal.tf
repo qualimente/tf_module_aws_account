da22478d07c831a43493b6e5dfbcda777081d19b
Stephen Kuenzli <skuenzli@qualimente.com>
2017-09-26 17:05:18 -0700

    Define a separate bucket for this module's testing.  Compute suffix so that bucket name does not end in hyphen.

--- config/test/fixtures/minimal/minimal.tf
+++ config/test/fixtures/minimal/minimal.tf
@@ -3,6 +3,6 @@
 module "it_minimal" {
   source = "../../../" //minimal integration test
 
-  aws_config_name   = "minimal"
-  aws_bucket_prefix = "qm-infra-module-ct-logs-2017-09-26"
+  aws_config_name   = "testing-skuenzli"
+  aws_bucket_prefix = "qm-infra-module-config-testing"
 }
