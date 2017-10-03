0c7a967886630a54fea926da415c1a11059c0890
Stephen Kuenzli <skuenzli@qualimente.com>
2017-10-03 14:42:26 -0700

    Update test to use a random suffix in bucket name to avoid collisions during testing.

--- cloudtrail/test/fixtures/minimal/minimal.tf
+++ cloudtrail/test/fixtures/minimal/minimal.tf
@@ -2,13 +2,18 @@
 
 data "aws_caller_identity" "current" {}
 
+resource "random_id" "cloudtrail" {
+  prefix      = "qm-test-cloudtrail-logs-"
+  byte_length = 4
+}
+
 module "it_minimal" {
   source = "../../../" //minimal integration test
 
   aws_account_id      = "${data.aws_caller_identity.current.account_id}"
   aws_region          = "us-west-2"
   aws_cloudtrail_name = "minimal"
-  s3_bucket_name      = "qm-infra-module-ct-logs-2017-09-07"
+  s3_bucket_name      = "${random_id.cloudtrail.hex}"
 
   // required to have a repeatable test
   s3_force_destroy = true
