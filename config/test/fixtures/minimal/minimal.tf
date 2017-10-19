37d0182dc01188bd70d17b9c2b9e3cb39baabd9f
Stephen Kuenzli <skuenzli@qualimente.com>
2017-10-18 23:05:38 -0700

    Use a random id to namespace each test deployment.

--- config/test/fixtures/minimal/minimal.tf
+++ config/test/fixtures/minimal/minimal.tf
@@ -1,8 +1,13 @@
 // instantiate the config module only supplying required parameters with the intent of really exercising the defaults
 
+resource "random_id" "testing_suffix" {
+  byte_length = 4
+}
+
+
 module "it_minimal" {
   source = "../../../" //minimal integration test
 
-  aws_config_name   = "testing-gjullianfk"
-  aws_bucket_prefix = "qm-infra-module-config-testing-2"
+  aws_config_name   = "test-${random_id.testing_suffix.hex}"
+  aws_bucket_prefix = "qm-infra-module-config-test-${random_id.testing_suffix.hex}"
 }
