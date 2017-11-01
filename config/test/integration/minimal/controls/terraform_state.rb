f5917015589431496c2c7e8b0c483675ceefe00a
Stephen Kuenzli <skuenzli@qualimente.com>
2017-10-31 20:54:29 -0700

    DRY-out verification of Config rule existence and naming.

--- config/test/integration/minimal/controls/terraform_state.rb
+++ config/test/integration/minimal/controls/terraform_state.rb
@@ -54,73 +54,24 @@ control 'terraform_state' do
     end
 
     describe 'aws managed config rules' do
-      describe 'IAM_PASSWORD_POLICY' do
-        aws_config_IAM_PASSWORD_POLICY_rule = resources['aws_config_config_rule.IAM_PASSWORD_POLICY']['primary']
-        aws_config_IAM_PASSWORD_POLICY_rule_attributes = aws_config_IAM_PASSWORD_POLICY_rule['attributes']
 
-        describe('set the name of the rule') do
-          subject { aws_config_IAM_PASSWORD_POLICY_rule_attributes['name'] }
-          it { is_expected.to match(/IAM_PASSWORD_POLICY-test-[\w]+/) }
-        end
-      end
+      [
+          'IAM_PASSWORD_POLICY',
+          'S3_BUCKET_LOGGING_ENABLED',
+          'S3_BUCKET_PUBLIC_READ_PROHIBITED',
+          'S3_BUCKET_PUBLIC_WRITE_PROHIBITED',
+          'S3_BUCKET_SSL_REQUESTS_ONLY',
+          'S3_BUCKET_VERSIONING_ENABLED',
+          'ACM_CERTIFICATE_EXPIRATION_CHECK',
+      ].each do |rule_name|
+        rule = resources["aws_config_config_rule.#{rule_name}"]['primary']
+        rule_attributes = rule['attributes']
 
-      describe 'S3_BUCKET_LOGGING_ENABLED' do
-        aws_config_S3_BUCKET_LOGGING_ENABLED_rule = resources['aws_config_config_rule.S3_BUCKET_LOGGING_ENABLED']['primary']
-        aws_config_S3_BUCKET_LOGGING_ENABLED_rule_attributes = aws_config_S3_BUCKET_LOGGING_ENABLED_rule['attributes']
-
-        describe('set the name of the rule') do
-          subject { aws_config_S3_BUCKET_LOGGING_ENABLED_rule_attributes['name'] }
-          it { is_expected.to match(/S3_BUCKET_LOGGING_ENABLED-test-[\w]+/) }
-        end
-      end
-
-      describe 'S3_BUCKET_PUBLIC_READ_PROHIBITED' do
-        aws_config_S3_BUCKET_PUBLIC_READ_PROHIBITED_rule = resources['aws_config_config_rule.S3_BUCKET_PUBLIC_READ_PROHIBITED']['primary']
-        aws_config_S3_BUCKET_PUBLIC_READ_PROHIBITED_rule_attributes = aws_config_S3_BUCKET_PUBLIC_READ_PROHIBITED_rule['attributes']
-
-        describe('set the name of the rule') do
-          subject { aws_config_S3_BUCKET_PUBLIC_READ_PROHIBITED_rule_attributes['name'] }
-          it { is_expected.to match(/S3_BUCKET_PUBLIC_READ_PROHIBITED-test-[\w]+/) }
-        end
-      end
-
-      describe 'S3_BUCKET_PUBLIC_WRITE_PROHIBITED' do
-        aws_config_S3_BUCKET_PUBLIC_WRITE_PROHIBITED_rule = resources['aws_config_config_rule.S3_BUCKET_PUBLIC_WRITE_PROHIBITED']['primary']
-        aws_config_S3_BUCKET_PUBLIC_WRITE_PROHIBITED_rule_attributes = aws_config_S3_BUCKET_PUBLIC_WRITE_PROHIBITED_rule['attributes']
-
-        describe('set the name of the rule') do
-          subject { aws_config_S3_BUCKET_PUBLIC_WRITE_PROHIBITED_rule_attributes['name'] }
-          it { is_expected.to match(/S3_BUCKET_PUBLIC_WRITE_PROHIBITED-test-[\w]+/) }
-        end
-      end
-
-      describe 'S3_BUCKET_SSL_REQUESTS_ONLY' do
-        aws_config_S3_BUCKET_SSL_REQUESTS_ONLY_rule = resources['aws_config_config_rule.S3_BUCKET_SSL_REQUESTS_ONLY']['primary']
-        aws_config_S3_BUCKET_SSL_REQUESTS_ONLY_rule_attributes = aws_config_S3_BUCKET_SSL_REQUESTS_ONLY_rule['attributes']
-
-        describe('set the name of the rule') do
-          subject { aws_config_S3_BUCKET_SSL_REQUESTS_ONLY_rule_attributes['name'] }
-          it { is_expected.to match(/S3_BUCKET_SSL_REQUESTS_ONLY-test-[\w]+/) }
-        end
-      end
-
-      describe 'S3_BUCKET_VERSIONING_ENABLED' do
-        aws_config_S3_BUCKET_VERSIONING_ENABLED_rule = resources['aws_config_config_rule.S3_BUCKET_VERSIONING_ENABLED']['primary']
-        aws_config_S3_BUCKET_VERSIONING_ENABLED_rule_attributes = aws_config_S3_BUCKET_VERSIONING_ENABLED_rule['attributes']
-
-        describe('set the name of the rule') do
-          subject { aws_config_S3_BUCKET_VERSIONING_ENABLED_rule_attributes['name'] }
-          it { is_expected.to match(/S3_BUCKET_VERSIONING_ENABLED-test-[\w]+/) }
-        end
-      end
-
-      describe 'ACM_CERTIFICATE_EXPIRATION_CHECK' do
-        aws_config_ACM_CERTIFICATE_EXPIRATION_CHECK_rule = resources['aws_config_config_rule.ACM_CERTIFICATE_EXPIRATION_CHECK']['primary']
-        aws_config_ACM_CERTIFICATE_EXPIRATION_CHECK_rule_attributes = aws_config_ACM_CERTIFICATE_EXPIRATION_CHECK_rule['attributes']
-        puts aws_config_ACM_CERTIFICATE_EXPIRATION_CHECK_rule_attributes
-        describe('set the name of the rule') do
-          subject { aws_config_ACM_CERTIFICATE_EXPIRATION_CHECK_rule_attributes['name'] }
-          it { is_expected.to match(/ACM_CERTIFICATE_EXPIRATION_CHECK-test-[\w]+/) }
+        describe("#{rule_name} rule") do
+          describe("name") do
+            subject { rule_attributes['name'] }
+            it { is_expected.to match(/#{Regexp.quote(rule_name)}-test-[\w]+/) }
+          end
         end
       end
     end
